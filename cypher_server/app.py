dd3bd126639b3e91772eadb346bbc7a57e0aca7e
ANDRES H CARRERA <andreshcar@live.com>
2019-02-05 11:02:02 -0700

    adding the inital files to the repo

---
+++ cypher_server/app.py
@@ -0,0 +1,37 @@
+from flask import Flask
+import cypher_app.cyphers as cyphers
+from cypher_datto import CypherDatto
+from flask import jsonify
+app = Flask(__name__)
+app.env = "Development"
+app.debug = True
+HOST = "localhost"
+PORT = 5000
+URL_TEMPLATE = "https://%s:%s/%s"
+
+available_cyphers = [CypherDatto(e, URL_TEMPLATE % (
+    HOST, PORT, e.name)) for e in cyphers.cyphers]
+
+
+@app.route("/<cypher_name>/encrypt/<message>/<key>")
+def encrypt_route(cypher_name, message, key):
+    pass
+
+
+@app.route("/<cypher_name>/decrypt/<message>/<key>")
+def decrypt_route(cypher_name, message, key):
+    pass
+
+
+@app.route("/")
+def primary_get_routes():
+    return jsonify(available_cyphers)
+
+
+@app.route("/<cypher_name>")
+def get_cypher(cypher_name):
+    pass
+
+
+if __name__ == "__main__":
+    app.run(port=PORT, host=HOST)
---
+++ cypher_server/cypher_datto.py
@@ -0,0 +1,37 @@
+class CypherDatto(object):
+    def __init__(self, module, url):
+        super(CypherDatto, self).__init__()
+        self.url = url
+        self.intergrate_module(module)
+
+    def intergrate_module(self, module):
+        self.module = module
+        self.name = module.name
+        self.key_type = module.key_type
+        self.description = module.description
+        self.example = module.example
+
+    def decrypt(self, message, key):
+        decrypted_output = None
+        if(self.key_type == "none"):
+            decrypted_output = self.module.decrypt(message)
+        else:
+            decrypted_output = self.module.decrypt(key, message)
+        return decrypted_output
+
+    def encrypt(self, message, key):
+        encrypted_output = None
+        if(self.key_type == "none"):
+            encrypted_output = self.module.encrypt(message)
+        else:
+            encrypted_output = self.module.encrypt(key, message)
+        return encrypted_output
+
+    def serialize(self):
+        return {
+            "name": self.name,
+            "keyType": self.key_type,
+            "description": self.description,
+            "example": self.example,
+            "url": self.url
+        }
